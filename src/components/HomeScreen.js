229d70f850d6a040ffc4b77353e07ce1de538331
sigidhanafi <sigidhanafi@gmail.com>
2017-10-27 20:55:44 +0700

    Add separator to view

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -10,8 +10,10 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   Image,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native'
 import { AppEventsLogger, LoginButton, AccessToken } from 'react-native-fbsdk'
 
@@ -40,124 +42,149 @@ class HomeScreen extends Component {
   constructor (props) {
     super(props)
     AppEventsLogger.logEvent('page', { name: 'home' })
+    this.state = {
+      login: false,
+      token: '',
+    }
+  }
+
+  componentDidMount () {
+    AccessToken.getCurrentAccessToken().then(
+      (data) => {
+        this.setState({ login: true, token: data.accessToken.toString() })
+        // alert(data.accessToken.toString())
+      }
+    )
   }
 
   render() {
     const { navigate } = this.props.navigation;    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        <Text style={styles.blue}>Fake-</Text>
-          <Text style={styles.blue}>O</Text>
-          <Text style={styles.green}>L</Text>
-          <Text style={styles.orange}>X</Text>
-          .co.id
-        </Text>
-        <View style={styles.row}>
-          <View style={styles.col}>
-            <TouchableOpacity
-              style={styles.categoryContainer}
-              onPress={() => navigate('Category', { title: 'Mobil', data: 'car'})}
-            >
+      <ScrollView style={styles.container}>
+        <View style={styles.bodyContainer}>
+          <Text style={styles.welcome}>
+          <Text style={styles.blue}>Fake-</Text>
+            <Text style={styles.blue}>O</Text>
+            <Text style={styles.green}>L</Text>
+            <Text style={styles.orange}>X</Text>
+            .co.id
+          </Text>
+          <View style={styles.row}>
+            <View style={styles.col}>
+              <TouchableOpacity
+                style={styles.categoryContainer}
+                onPress={() => navigate('Category', { title: 'Mobil', data: 'car'})}
+              >
+                <Image
+                  source={carIcon}
+                  style={styles.icon}
+                />
+                <Text>Mobil</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.col}>
+              <TouchableOpacity
+                style={styles.categoryContainer}
+                onPress={() => navigate('Category', { title: 'Motor', data: 'motorcycle'})}
+              >
+                <Image
+                  source={vespaIcon}
+                  style={styles.icon}
+                />
+                <Text>Motor</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.col}>
               <Image
-                source={carIcon}
+                source={bicycleIcon}
                 style={styles.icon}
               />
-              <Text>Mobil</Text>
-            </TouchableOpacity>
+              <Text>Sepeda</Text>
+            </View>
           </View>
-          <View style={styles.col}>
-            <TouchableOpacity
-              style={styles.categoryContainer}
-              onPress={() => navigate('Category', { title: 'Motor', data: 'motorcycle'})}
-            >
+
+          <View style={styles.row}>
+            <View style={styles.col}>
               <Image
-                source={vespaIcon}
+                source={computerIcon}
                 style={styles.icon}
               />
-              <Text>Motor</Text>
-            </TouchableOpacity>
+              <Text>Komputer/ Laptop</Text>
+            </View>
+            <View style={styles.col}>
+              <Image
+                source={phoneIcon}
+                style={styles.icon}
+              />
+              <Text>Smartphone</Text>
+            </View>
+            <View style={styles.col}>
+              <Image
+                source={cameraIcon}
+                style={styles.icon}
+              />
+              <Text>Kamera</Text>
+            </View>
           </View>
-          <View style={styles.col}>
-            <Image
-              source={bicycleIcon}
-              style={styles.icon}
-            />
-            <Text>Sepeda</Text>
-          </View>
-        </View>
 
-        <View style={styles.row}>
-          <View style={styles.col}>
-            <Image
-              source={computerIcon}
-              style={styles.icon}
-            />
-            <Text>Komputer/ Laptop</Text>
+          <View style={styles.row}>
+            <View style={styles.col}>
+              <Image
+                source={homeIcon}
+                style={styles.icon}
+              />
+              <Text>Rumah</Text>
+            </View>
+            <View style={styles.col}>
+              <Image
+                source={sofaIcon}
+                style={styles.icon}
+              />
+              <Text>Furnitur</Text>
+            </View>
+            <View style={styles.col}>
+              <Image
+                source={tshirtIcon}
+                style={styles.icon}
+              />
+              <Text>Pakaian</Text>
+            </View>
           </View>
-          <View style={styles.col}>
-            <Image
-              source={phoneIcon}
-              style={styles.icon}
-            />
-            <Text>Smartphone</Text>
-          </View>
-          <View style={styles.col}>
-            <Image
-              source={cameraIcon}
-              style={styles.icon}
-            />
-            <Text>Kamera</Text>
-          </View>
-        </View>
+          
+          <View style={styles.separator} />
 
-        <View style={styles.row}>
-          <View style={styles.col}>
-            <Image
-              source={homeIcon}
-              style={styles.icon}
-            />
-            <Text>Rumah</Text>
-          </View>
-          <View style={styles.col}>
-            <Image
-              source={sofaIcon}
-              style={styles.icon}
-            />
-            <Text>Furnitur</Text>
-          </View>
-          <View style={styles.col}>
-            <Image
-              source={tshirtIcon}
-              style={styles.icon}
-            />
-            <Text>Pakaian</Text>
-          </View>
-        </View>
-        
-        <Text style={styles.instructions}>
-          Login untuk mendapatkan penawaran diskon menarik!
-        </Text>
+          {!this.state.login ? (
+            <Text style={styles.instructions}>
+              Jadi member untuk mendapatkan penawaran diskon menarik!
+            </Text>
+          ) : (
+            <Text style={styles.instructions}>
+              Selamat! Dengan bergabung menjadi member, Anda berhak mendapatkan diskon hingga 70%.
+            </Text>
+          )}
 
-        <LoginButton
-          publishPermissions={["publish_actions"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    alert(data.accessToken.toString())
-                  }
-                )
+          <LoginButton
+            publishPermissions={["publish_actions"]}
+            onLoginFinished={
+              (error, result) => {
+                if (error) {
+                  alert("login has error: " + result.error);
+                } else if (result.isCancelled) {
+                  alert("login is cancelled.");
+                } else {
+                  AccessToken.getCurrentAccessToken().then(
+                    (data) => {
+                      this.setState({ login: true, token: data.accessToken.toString() })
+                      // alert(data.accessToken.toString())
+                    }
+                  )
+                }
               }
             }
-          }
-          onLogoutFinished={() => alert("logout.")}/>
-      </View>
+            onLogoutFinished={() => alert("logout.")}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -167,9 +194,12 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  bodyContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
   },
   welcome: {
     fontSize: 20,
@@ -223,5 +253,12 @@ const styles = StyleSheet.create({
     color: '#FD8324',
     fontSize: 50,
     fontWeight: 'bold',
+  },
+  separator: {
+    marginTop: 50,
+    width: Dimensions.get('window').width / 2,
+    height: 1,
+    borderWidth: 0.2,
+    borderColor: '#FD8324',
   }
 });
